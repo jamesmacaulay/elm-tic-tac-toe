d1821622e327b58ad8c182a28a23d467e6b5975f
James MacAulay <jmacaulay@gmail.com>
2017-02-05 18:01:00 -0500

    reorg + draw state

--- TicTacToe.elm
+++ TicTacToe.elm
@@ -6,61 +6,7 @@ import Html.Events exposing (..)
 import Dict exposing (Dict)
 
 
-main : Program Never Model Msg
-main =
-    Html.beginnerProgram
-        { model = newGame
-        , view = view
-        , update = update
-        }
-
-
-type Symbol
-    = X
-    | O
-
-
-type alias Model =
-    Dict Int Symbol
-
-
-type PlayState
-    = CurrentPlayer Symbol
-    | Winner Symbol
-
-
-newGame : Model
-newGame =
-    Dict.empty
-
-
-viewSpace : ( Int, Maybe Symbol ) -> Html Msg
-viewSpace ( index, space ) =
-    td
-        [ style
-            [ ( "background-color", "#eef" )
-            , ( "text-align", "center" )
-            , ( "font-size", "72px" )
-            , ( "font-family", "Helvetica" )
-            , ( "width", "100px" )
-            , ( "height", "100px" )
-            ]
-        , onClick (Mark index)
-        ]
-        [ space
-            |> Maybe.map toString
-            |> Maybe.withDefault ""
-            |> text
-        ]
-
-
-viewRow : List ( Int, Maybe Symbol ) -> Html Msg
-viewRow indexedSpaces =
-    let
-        tableCells =
-            List.map viewSpace indexedSpaces
-    in
-        tr [] tableCells
+-- UTIL
 
 
 splitBy : Int -> List a -> List (List a)
@@ -71,66 +17,68 @@ splitBy n xs =
         List.take n xs :: splitBy n (List.drop n xs)
 
 
-indexRange : List Int
-indexRange =
-    List.range 0 8
+
+-- PROGRAM
 
 
-viewBoard : Model -> Html Msg
-viewBoard model =
+main : Program Never Model Msg
+main =
+    Html.beginnerProgram
+        { model = newGame
+        , update = update
+        , view = view
+        }
+
+
+
+-- MODEL
+
+
+type alias Model =
+    Dict Int Symbol
+
+
+type Symbol
+    = X
+    | O
+
+
+type PlayState
+    = CurrentPlayer Symbol
+    | Winner Symbol
+    | Draw
+
+
+newGame : Model
+newGame =
+    Dict.empty
+
+
+playState : Model -> PlayState
+playState model =
     let
-        tableRows =
-            indexRange
-                |> List.map (flip Dict.get model)
-                |> List.map2 (,) indexRange
-                |> splitBy 3
-                |> List.map viewRow
+        currentPlayer =
+            if Dict.size model % 2 == 0 then
+                X
+            else
+                O
+
+        default =
+            if Dict.size model == 9 then
+                Draw
+            else
+                CurrentPlayer currentPlayer
     in
-        table [] tableRows
+        boardWinner model
+            |> Maybe.map Winner
+            |> Maybe.withDefault default
 
 
-viewPlayState : PlayState -> Html Msg
-viewPlayState playState =
-    let
-        message =
-            case playState of
-                CurrentPlayer symbol ->
-                    "Current player: " ++ toString symbol
-
-                Winner symbol ->
-                    toString symbol ++ " wins!"
-    in
-        p [] [ text message ]
-
-
-view : Model -> Html Msg
-view model =
-    div
-        [ style
-            [ ( "margin", "0 20px" )
-            , ( "font-family", "Helvetica" )
-            ]
-        ]
-        [ h1 [] [ text "Tic-Tac-Toe" ]
-        , viewPlayState (playState model)
-        , viewBoard model
-        , button [ onClick Reset ] [ text "Reset" ]
-        ]
-
-
-type Msg
-    = Mark Int
-    | Reset
-
-
-update : Msg -> Model -> Model
-update msg model =
-    case msg of
-        Mark i ->
-            mark i model
-
-        Reset ->
-            newGame
+boardWinner : Model -> Maybe Symbol
+boardWinner model =
+    winningLines
+        |> List.filterMap (lineWinner model)
+        |> List.head
 
 
 winningLines : List (List Int)
@@ -159,35 +107,114 @@ lineWinner model line =
             Nothing
 
 
-boardWinner : Model -> Maybe Symbol
-boardWinner model =
-    winningLines
-        |> List.filterMap (lineWinner model)
-        |> List.head
+
+-- UPDATE
 
 
-playState : Model -> PlayState
-playState model =
-    let
-        currentPlayer =
-            if Dict.size model % 2 == 0 then
-                X
-            else
-                O
-    in
-        boardWinner model
-            |> Maybe.map Winner
-            |> Maybe.withDefault (CurrentPlayer currentPlayer)
+type Msg
+    = Mark Int
+    | Reset
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Mark i ->
+            mark i model
+
+        Reset ->
+            newGame
 
 
 mark : Int -> Model -> Model
 mark i model =
     case playState model of
-        Winner _ ->
-            model
-
         CurrentPlayer symbol ->
             if Dict.member i model then
                 model
             else
                 Dict.insert i symbol model
+
+        _ ->
+            model
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    div
+        [ style
+            [ ( "margin", "0 20px" )
+            , ( "font-family", "Helvetica" )
+            ]
+        ]
+        [ h1 [] [ text "Tic-Tac-Toe" ]
+        , viewPlayState (playState model)
+        , viewBoard model
+        , button [ onClick Reset ] [ text "Reset" ]
+        ]
+
+
+viewPlayState : PlayState -> Html Msg
+viewPlayState playState =
+    let
+        message =
+            case playState of
+                CurrentPlayer symbol ->
+                    "Current player: " ++ toString symbol
+
+                Winner symbol ->
+                    toString symbol ++ " wins!"
+
+                Draw ->
+                    "Draw"
+    in
+        p [] [ text message ]
+
+
+viewBoard : Model -> Html Msg
+viewBoard model =
+    let
+        indexRange =
+            List.range 0 8
+
+        tableRows =
+            indexRange
+                |> List.map (flip Dict.get model)
+                |> List.map2 (,) indexRange
+                |> splitBy 3
+                |> List.map viewRow
+    in
+        table [] tableRows
+
+
+viewRow : List ( Int, Maybe Symbol ) -> Html Msg
+viewRow indexedSpaces =
+    let
+        tableCells =
+            List.map viewSpace indexedSpaces
+    in
+        tr [] tableCells
+
+
+viewSpace : ( Int, Maybe Symbol ) -> Html Msg
+viewSpace ( index, space ) =
+    td
+        [ style
+            [ ( "background-color", "#eef" )
+            , ( "text-align", "center" )
+            , ( "font-size", "72px" )
+            , ( "font-family", "Helvetica" )
+            , ( "width", "100px" )
+            , ( "height", "100px" )
+            ]
+        , onClick (Mark index)
+        ]
+        [ space
+            |> Maybe.map toString
+            |> Maybe.withDefault ""
+            |> text
+        ]
